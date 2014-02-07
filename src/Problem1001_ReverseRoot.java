934a995bb3729f2c00661da2d340793a209aa931
Justin <e@>
2014-02-06 17:21:21 -0800

    Added 1001.

---
+++ src/Problem1001_ReverseRoot.java
@@ -0,0 +1,27 @@
+/**
+ * Created by Justin on 2/6/14.
+ */
+import java.io.*;
+import java.util.*;
+
+public class Problem1001_ReverseRoot
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+        double[] input = new double[131072];
+        int top = 0;
+        while (in.hasNextDouble() && top < input.length) {
+            double a = in.nextDouble();
+            input[top] = a;
+            top++;
+        }
+        in.close();
+        for (int i = top - 1; i >= 0; i--) {
+            out.printf("%.4f", Math.sqrt(input[i]));
+            out.println();
+            out.flush();
+        }
+    }
+}
