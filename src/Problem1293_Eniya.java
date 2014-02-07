eb80c625eae2e564861401b2d051fc8d64961efe
Justin Eng <engjustin3@gmail.com>
2014-02-06 21:52:23 -0800

    Did 1293.

---
+++ src/Problem1293_Eniya.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Justin on 2/6/14.
+ */
+import java.awt.*;
+import java.io.*;
+import java.util.*;
+public class Problem1293_Eniya {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+        int weight = in.nextInt() * in.nextInt() * in.nextInt() * 2;
+        out.print(weight);
+        out.flush();
+    }
+}
